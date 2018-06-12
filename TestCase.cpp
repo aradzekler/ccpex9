8d36653176a78dc8f981cd20e7756a65853f2b2e
Arad Zekler <25280958+aradzekler@users.noreply.github.com>
2018-06-12 12:52:53 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,5 +1,15 @@
 #include "TestCase.hpp"
 
+
+
+TestCase::TestCase(string name, ostream &os):stream(os.rdbuf()) // constructor
+{
+    testcase = name;
+    total = 0;
+    failed = 0;
+    passed = 0;
+}
+
 ostream& operator<<(ostream& os, TestCase& obj)
 {
     return obj.stream;
