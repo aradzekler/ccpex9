7430d3a82dd20920b2d34a887be1333c0be550a7
Arad Zekler <25280958+aradzekler@users.noreply.github.com>
2018-06-12 12:42:55 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -5,7 +5,7 @@ ostream& operator<<(ostream& os, TestCase& obj)
     return obj.stream;
 }
 
-void print()
+void TestCase::print()
 {
     stream << testcase << ": " << failed << " tests failed, " << passed << " tests passed, " << total << " total." << endl << endl;
     cout << *this;
